722d2bf5fd18c10a07f6cd747ffc2d43794e16c8
shahd-work <hamammichahd@gmail.com>
2026-04-12 20:52:16 +0100

    Merge server.js and User.js

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+// Routes
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
+const recipeRoutes = require('./routes/recipeRoutes');
+app.use('/api/recipes', recipeRoutes);
+
+// Connect to MongoDB then start server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
