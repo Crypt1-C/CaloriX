d2751ab74bb29f216ba1ae6a595bb5bab49a4c13
shahd-work <hamammichahd@gmail.com>
2026-04-26 14:44:19 +0100

    suggestion mekla

--- server.js
+++ server.js
@@ -26,3 +26,5 @@ mongoose.connect(process.env.MONGO_URI)
     });
   })
   .catch(err => console.error('DB connection error:', err));
+  const suggestionRoutes = require('./routes/suggestionRoutes');
+app.use('/api/suggestions', suggestionRoutes);
